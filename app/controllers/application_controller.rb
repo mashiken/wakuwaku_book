6c3eeddee53c85cf968c384feb8177388055e99c
mashiken <tatsumikogorou@gmail.com>
2020-06-26 00:11:35 +0900

    fix sign_up/views app/controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+	def after_sign_up_path_for(resource)
+  end
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,keys:[:family_name, :given_name, :family_name_kana, :given_name_kana, :phone_number])
+  end
 end
