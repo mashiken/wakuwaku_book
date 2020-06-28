b679f607ee02a5ab5090ec0a2df7a25b35558812
mashiken <tatsumikogorou@gmail.com>
2020-06-28 16:23:58 +0900

    set/RDS install/gem mysql2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,6 +45,7 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
+  config.action_cable.allowed_request_origins = [ 'http://xx.xx.xx.xx/' ]
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
