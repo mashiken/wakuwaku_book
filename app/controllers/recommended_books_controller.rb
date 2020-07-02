a334f1d0b01018393845720a2b92759683d7f8fe
mashiken <tatsumikogorou@gmail.com>
2020-07-02 17:04:20 +0900

    fix recommended_book/create

--- app/controllers/recommended_books_controller.rb
+++ app/controllers/recommended_books_controller.rb
@@ -1,10 +1,7 @@
 # frozen_string_literal: true
-
+require 'pry'
 class RecommendedBooksController < ApplicationController
   def index
-  end
-
-  def create
   end
 
   def confirm
@@ -23,12 +20,32 @@ class RecommendedBooksController < ApplicationController
       end
       params[:keyword]
     end
-
   end
 
   def finish
+    #オススメされたユザーIDを取得
+    @recommended_user = User.find(params[:id])
+    @book_details = RakutenWebService::Books::Book.search(isbn: params[:book_id])
+    @recommended_book = RecommendedBook.new
+  end
+
+  def create
+    recommended_book = RecommendedBook.new(recommended_book_params)
+    recommended_book.user_id = current_user.id
+    if recommended_book.save
+      redirect_to action: :index and return
+    else
+      redirect_to action: :finish and return
+    end
   end
 
   def destroy
   end
+
+  private
+
+  def recommended_book_params
+    params.require(:recommended_book).permit(:user_id,:recommended_user_id, :book_id,:title,:text)
+  end
+
 end
