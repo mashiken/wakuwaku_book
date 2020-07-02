3b8f5a5c81e6e6d9e84215a74f446ab7755ccac3
mashiken <tatsumikogorou@gmail.com>
2020-07-02 17:21:05 +0900

    fix book_shelves/create

--- app/controllers/book_shelves_controller.rb
+++ app/controllers/book_shelves_controller.rb
@@ -1,7 +1,9 @@
+require "pry"
 class BookShelvesController < ApplicationController
-
+  
   def create
-		book_shelf = BookShelf.new
+  	    binding.pry
+		book_shelf = BookShelf.new(book_shelves_params)
 		book_shelves = BookShelf.where(user_id: current_user.id)
 
     book_shelves.each do |book_shelf|
@@ -13,7 +15,7 @@ class BookShelvesController < ApplicationController
 		#本棚に整理出来る書籍の数は10冊(user_id.count<=10)
 		if book_shelves.count <= 9
 			book_shelf.user_id = current_user.id  #user_id
-			book_shelf.book_id = params[:book_id] #book_id
+			#book_shelf.book_id = params[:book_id] #book_id
 			book_shelf.save
 			redirect_to user_path(current_user)  and return
 		else
@@ -21,11 +23,15 @@ class BookShelvesController < ApplicationController
 		end
 	end
 
-  def destroy
-    book_shelf = BookShelf.find_by(params[:id])
-    book_shelf.destroy
-    redirect_to user_path(current_user)
-  end
+    def destroy
+	    book_shelf = BookShelf.find_by(params[:id])
+	    book_shelf.destroy
+	    redirect_to user_path(current_user)
+    end
 
   private
+
+    def book_shelves_params
+		params.require(:book_shelf).permit(:user_id, :book_id)
+	end
 end
