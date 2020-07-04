b6ce7f08045c2f82cfcf95b6f2dfb0900ca76511
mashiken <tatsumikogorou@gmail.com>
2020-07-04 21:41:42 +0900

    fix search/books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
+require "pry"
 class BooksController < ApplicationController
-
   def index
   	if params[:keyword]
   		if params[:search_condition] == "title"
@@ -7,6 +7,21 @@ class BooksController < ApplicationController
   		elsif params[:search_condition] == "author"
   			@books = RakutenWebService::Books::Book.search(author: params[:keyword])
   		end
+
+      #検索結果にgem kaminariにてpagenationを追加する。
+      @books_full = [] #配列を用意
+      page = 1
+
+      while page <= 5 do #どんなに大量の検索結果でも10ページまでに制限
+      last_items = @books.page(page)
+        last_items.all do |item|
+          @books_full.push(item)
+        end
+      end
+
+      if @books_full.present?
+        @books = Kaminari.paginate_array(@books_full, total_count: @books_full.count ).page(params[:page]).per(30)
+      end
   	end
   end
 
