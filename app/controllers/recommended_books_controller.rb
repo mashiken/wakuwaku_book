ffffa0eaf83e663b74c6efb0ff5f73eaa6445604
mashiken <tatsumikogorou@gmail.com>
2020-07-04 23:51:47 +0900

    fix searchbook/pagenation

--- app/controllers/recommended_books_controller.rb
+++ app/controllers/recommended_books_controller.rb
@@ -3,7 +3,6 @@
 class RecommendedBooksController < ApplicationController
 
   def show
-
     @user = User.find(params[:id])
     @reviews = Review.where(user_id: params[:id])
     @recommended = RecommendedBook.where(user_id: params[:id])
@@ -26,13 +25,22 @@ class RecommendedBooksController < ApplicationController
     #レビュー一覧から書籍を選ぶ場合取得
     @reviews = Review.where(user_id: current_user.id)
 
-    if params[:keyword] #オススメ確認画面内にてオススメ書籍を検索するのに使用
+    if params[:keyword]
       if params[:search_condition] == "title"
-        @books = RakutenWebService::Books::Book.search(title: params[:keyword])
+        books = RakutenWebService::Books::Book.search(title: params[:keyword])
       elsif params[:search_condition] == "author"
-        @books = RakutenWebService::Books::Book.search(author: params[:keyword])
+        books = RakutenWebService::Books::Book.search(author: params[:keyword])
       end
-      params[:keyword]
+
+      #検索結果にgem kaminariにてpagenationを追加する。
+      @books_full = [] #配列を用意
+      books.all.each do |book| #books.allで検索結果の全ての商品単位を表示
+        @books_full.push(book)
+      end
+
+      if @books_full.present?
+        @books = Kaminari.paginate_array(@books_full, total_count: @books_full.count ).page(params[:page])
+      end
     end
   end
 
