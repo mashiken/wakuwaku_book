af984b7c1bd74b2735077b125c17022ff6af13b2
mashiken <tatsumikogorou@gmail.com>
2020-07-07 21:02:27 +0900

    fix BookSearch/controller views

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,32 +1,41 @@
 
 class BooksController < ApplicationController
   def index
-  	if params[:keyword] == "" #キーワード空欄の場合
+  	if params[:keyword] == ""
       flash.now[:notice] = '申し訳ございません。お探しの商品が見つかりませんでした。もう一度、ラジオボタン「書籍タイトル」または「著者名」にチェックの上、検索ワードの入力をお願いします。'
     else
-      @books_full = [] #配列を用意
-  		if params[:search_condition] == "title"
-  		  books = RakutenWebService::Books::Book.search(title: params[:keyword])
-        #検索結果にgem kaminariにてpagenationを追加する。
-        books.all.each do |book| #books.allで検索結果の全ての商品単位を表示
-          @books_full.push(book)
-        end
-  		elsif params[:search_condition] == "author"
-  			books = RakutenWebService::Books::Book.search(author: params[:keyword])
-        #検索結果にgem kaminariにてpagenationを追加する。
-        books.all.each do |book| #books.allで検索結果の全ての商品単位を表示
-          @books_full.push(book)
-        end
-      else
-        flash.now[:notice] = '申し訳ございません。お探しの商品が見つかりませんでした。もう一度、ラジオボタン「書籍タイトル」または「著者名」にチェックの上、検索ワードの入力をお願いします。'
-  		end
+      if params[:search_condition] == "title"
 
-      if @books_full.present?
-        @books = Kaminari.paginate_array(@books_full, total_count: @books_full.count ).page(params[:page])
-      else
-        flash.now[:notice] = '申し訳ございません。お探しの商品が見つかりませんでした。もう一度、ラジオボタン「書籍タイトル」または「著者名」にチェックの上、検索ワードの入力をお願いします。'
+        books = RakutenWebService::Books::Book.search(title: params[:keyword]) #titleで検索
+        @books_full = []
+        #配列を用意(kaminari/pagenation)
+
+        books.all.each do |book|   #books.allで検索結果の全ての商品単位を表示
+          @books_full.push(book)   #books.allを切り分けたbookを1つずつ配列へ挿入
+        end
+
+        if @books_full.present?    #結果として配列にデータがあるか？あればpagination設定 無しならErrorMessage
+          @books = Kaminari.paginate_array(@books_full, total_count: @books_full.count ).page(params[:page])
+        else
+          flash.now[:notice] = '申し訳ございません。お探しの商品が見つかりませんでした。もう一度、検索ワードの入力をお願いします。'
+        end
+
+      elsif params[:search_condition] == "author"
+        books = RakutenWebService::Books::Book.search(author: params[:keyword]) #authorで検索
+        @books_full = []
+        #配列を用意(kaminari/pagenation)
+
+        books.all.each do |book|   #books.allで検索結果の全ての商品単位を取得
+          @books_full.push(book)   #books.allを切り分けたbookを1つずつ配列へ挿入
+        end
+
+        if @books_full.present?    #結果として配列にデータがあるか？あればpagination設定 無しならErrorMessage
+          @books = Kaminari.paginate_array(@books_full, total_count: @books_full.count ).page(params[:page])
+        else
+          flash.now[:notice] = '申し訳ございません。お探しの商品が見つかりませんでした。もう一度、検索ワードの入力をお願いします。'
+        end
       end
-  	end
+    end
   end
 
   def show
