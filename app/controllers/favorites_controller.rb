beb18d7dad3e842175a84a7f70164f80370e5cc3
mashiken <tatsumikogorou@gmail.com>
2020-07-11 20:39:20 +0900

    favorite add

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,14 +1,26 @@
+
 class FavoritesController < ApplicationController
   before_action :authenticate_user!
+
   def create
-  	@review = Review.find(params[:review_user_id])
-  	favorite = current_user.favorites.new(book_id: @review.id)
+  	@review = Review.find(params[:review_id])
+  	favorite = current_user.favorites.new(review_id: @review.id)
   	favorite.save
+    #非同期通信に必要な変数
+  	@user = User.find(@review.user_id)
+    @reviews = Review.where(user_id: @review.user_id).page(params[:page]).reverse_order.per(10)
+    @recommended = RecommendedBook.where(user_id: @review.user_id)
+    @recommended_user = RecommendedBook.where(recommended_user_id: @review.user_id)
   end
 
   def destroy
-  	@review = Review.find(params[:review_user_id])
-  	favorite = current_user.favorites.new(book_id: @review.id)
+  	@review = Review.find(params[:review_id])
+  	favorite = current_user.favorites.find_by(review_id: @review.id)
     favorite.destroy
+    #非同期通信に必要な変数
+  	@user = User.find(@review.user_id)
+    @reviews = Review.where(user_id: @review.user_id).page(params[:page]).reverse_order.per(10)
+    @recommended = RecommendedBook.where(user_id: @review.user_id)
+    @recommended_user = RecommendedBook.where(recommended_user_id: @review.user_id)
   end
 end
--- db/migrate/20200625083525_create_favorites.rb
+++ db/migrate/20200625083525_create_favorites.rb
@@ -3,8 +3,8 @@
 class CreateFavorites < ActiveRecord::Migration[5.2]
   def change
     create_table :favorites do |t|
-      t.integer  :user_id, null: false
-      t.integer  :recommended_user_id, null: false
+      t.integer  :user_id
+      t.integer  :recommended_user_id
 
       t.timestamps
     end
