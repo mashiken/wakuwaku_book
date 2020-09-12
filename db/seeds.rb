223ab13a65eed8fa925888eb3a515bdc1b6cc56c
mashiken <tatsumikogorou@gmail.com>
2020-09-12 10:02:54 +0900

    Fix pagination

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,14 +8,39 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+## Userのダミーデーター
+#50.times do |n|
+#  email = "wakuwaku2-#{n+1}@sakura"
+#  password = "password"
+#  User.create!(nickname: Faker::Name.name,
+#               email: email,
+#               password:              password,
+#               password_confirmation: password)
+#end
+#
+## レビューのダミーデーター
+#50.times do |n|
+#	Review.create!(user_id: 1,
+#		           book_id: "9784065177792",
+#		           title: Faker::Lorem.characters(5),
+#		           text: Faker::Lorem.characters(20), )
+#end
+
+# オススメ数ダミーデーター
 50.times do |n|
-  email = "wakuwaku2-#{n+1}@sakura"
-  password = "password"
-  User.create!(nickname: Faker::Name.name,
-               email: email,
-               password:              password,
-               password_confirmation: password)
+	RecommendedBook.create!(user_id: 1,
+		                    recommended_user_id: 2,
+		                    book_id: "9784065177792",
+		                    title: Faker::Lorem.characters(5),
+		                    text:Faker::Lorem.characters(20))
 end
 
-
-AdminUser.create!(email: 'admin@example.com', password: '33333333', password_confirmation: '33333333')
+# オススメされ数ダミーデーター
+50.times do |n|
+	RecommendedBook.create!(user_id: 2,
+		                    recommended_user_id: 1,
+		                    book_id: "9784906993871",
+		                    title: Faker::Lorem.characters(5),
+		                    text:Faker::Lorem.characters(20))
+end
+#AdminUser.create!(email: 'admin@example.com', password: '33333333', password_confirmation: '33333333')
